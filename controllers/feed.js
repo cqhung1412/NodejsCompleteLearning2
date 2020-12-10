9406059b7e581f3831eef3528be67947a3c4ccdc
Bear <wokhug99@gmail.com>
2020-12-10 17:29:28 +0700

    Update feed.js

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,10 @@
 exports.getPosts = (req, res, next) => {
-  
+  res.status(200).json({
+    posts: [
+      {
+        title: '1st Post',
+        content: 'This is the first post!!!'
+      }
+    ]
+  });
 };
